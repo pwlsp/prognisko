54945d97de77b46b934e94b2074d9f398e202db4
pwlk-hp <spychala.paw@gmail.com>
2024-05-22 13:01:20 +0200

    jednak teraz dopiero zestaw 1 zadanie 6 jest git bo nie miało funkcji

--- 5_powtorka_materialu/1_6.c
+++ 5_powtorka_materialu/1_6.c
@@ -2,14 +2,44 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main() {
+void oblicz_R(int **A, int **B, int **R, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (A[i][j] != B[i][j])
+                R[i][j] = 0;
+            else
+                R[i][j] = 1;
+        }
+    }
+}
+void oblicz_P(int **A, int **B, int **P, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (A[i][j] <= B[i][j])
+                P[i][j] = 0;
+            else
+                P[i][j] = 1;
+        }
+    }
+}
+
+int main()
+{
     FILE *wejA = fopen("wejscie_1_6_A.txt", "r");
-    if (wejA == NULL) {
+    if (wejA == NULL)
+    {
         printf("Błąd wejścia A");
         return 1;
     }
     FILE *wejB = fopen("wejscie_1_6_B.txt", "r");
-    if (wejB == NULL) {
+    if (wejB == NULL)
+    {
         printf("Błąd wejścia B");
         return 1;
     }
@@ -19,32 +49,42 @@ int main() {
     fscanf(wejA, "%d %d", &n, &m);
     fscanf(wejB, "%d %d", &nb, &mb);
 
-    if (n != nb || m != mb) {
+    if (n != nb || m != mb)
+    {
         printf("Podane wielkości macierzy A i B różnią się");
         return 1;
     }
 
+    int **A = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = (int *)malloc(m * sizeof(int));
+    }
+    int **B = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++)
+    {
+        B[i] = (int *)malloc(m * sizeof(int));
+    }
     int **R = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         R[i] = (int *)malloc(m * sizeof(int));
     }
     int **P = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         P[i] = (int *)malloc(m * sizeof(int));
     }
 
-    // Read the matrix from the file
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            int A, B;
-            fscanf(wejA, "%d", &A);
-            fscanf(wejB, "%d", &B);
-            if(A != B) R[i][j] = 0;
-            else R[i][j] = 1;
-            if(A <= B) P[i][j] = 0;
-            else P[i][j] = 1;
-            
-            // if(A<=B)
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int Aa, Bb;
+            fscanf(wejA, "%d", &Aa);
+            A[i][j] = Aa;
+            fscanf(wejB, "%d", &Bb);
+            B[i][j] = Bb;
 
             // R[i][j] = A;
         }
@@ -55,29 +95,72 @@ int main() {
 
     // Wypisywanie macierzy
     printf("Program na wejściu pobiera dwa pliki, których pierwsze wiersze zawierają n i m.\n\n");
+    printf("A:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("B:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", B[i][j]);
+        }
+        printf("\n");
+    }
+
+    oblicz_P(A, B, P, n, m);
+    oblicz_R(A, B, R, n, m);
+
+    printf("\n");
     printf("R:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             printf("%d ", R[i][j]);
         }
         printf("\n");
     }
     printf("\nP:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             printf("%d ", P[i][j]);
         }
         printf("\n");
     }
 
-    for (int i = 0; i < n; i++) {
+    // zwalnianie pamięci
+    for (int i = 0; i < n; i++)
+    {
+        free(A[i]);
+    }
+    free(A);
+    for (int i = 0; i < n; i++)
+    {
+        free(B[i]);
+    }
+    free(B);
+    for (int i = 0; i < n; i++)
+    {
         free(R[i]);
     }
     free(R);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         free(P[i]);
     }
     free(P);
+
+    // printf("\ntest\n");
 
     return 0;
 }
